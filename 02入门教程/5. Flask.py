c1452fc51c1d5a3ea87a3275bd6dd84802b115b3
Roisin <32283868+yangruixia@users.noreply.github.com>
2023-07-01 22:59:41 +0800

    Update 5. Flask.py

--- 02入门教程/5. Flask.py
+++ 02入门教程/5. Flask.py
@@ -1,1 +1,12 @@
 如果进行网站开发，需要学习Flask。
+# 1. Flask简介
+
+
+# 2. Flask入门
+
+## 2.1 Flask入门推荐教程
+- [B站视频](https://www.bilibili.com/video/BV1Hv41147ZS?p=1&vd_source=6477faa6e88a16768990ef3cb794d8eb)：入门可以只看前四p，后面不看。如果想学习项目部署，可以看p10的blueprints。如果跟不上或者不懂，就在网上查别的帖子。
+- [文档教程](https://tutorial.helloflask.com/hello/)
+- [相关帖子](https://zhuanlan.zhihu.com/p/80899085)
+
+
